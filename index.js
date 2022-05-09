9cc75bb4f7f1653c6d2502f7390364f75feffa69
stefano-franco <stefano.franco@nuvibit.com>
2022-05-09 09:05:29 +0200

    fix: updating packages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
-      '@semantic-release/changelog@6.0.0',
-      '@semantic-release/git@10.0.0',
-      'conventional-changelog-conventionalcommits@4.6.3'
+      '@semantic-release/changelog',
+      '@semantic-release/git',
+      'conventional-changelog-conventionalcommits'
     ],
 };
